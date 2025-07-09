0236334afe0ff10b59c38cf3a044fa58c222a80d
Gosqu248 <grzegorzurban248@gmail.com>
2025-07-09 23:28:04 +0200

    Revert "Move JWT key configuration to .env file and load with dotenv-java"
    This reverts commit 04a3c52142134c96043d76c16dd584244385dcca.

--- backend/src/main/java/com/urban/backend/BackendApplication.java
+++ backend/src/main/java/com/urban/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.urban.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure()
-                .filename(".env")
-                .load();
-        // wrzuć do System properties, żeby Spring je widział
-        System.setProperty("AUTH_PRIVATE_KEY", dotenv.get("AUTH_PRIVATE_KEY"));
-        System.setProperty("AUTH_PUBLIC_KEY",  dotenv.get("AUTH_PUBLIC_KEY"));
-
         SpringApplication.run(BackendApplication.class, args);
     }
 
