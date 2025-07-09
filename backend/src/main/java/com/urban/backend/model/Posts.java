6ef917d5a36ed0ab3091ed8e3db4929425145f90
Gosqu248 <grzegorzurban248@gmail.com>
2025-07-09 17:21:25 +0200

    Create initial database structure with entities for user, membership, posts, and related models

---
+++ backend/src/main/java/com/urban/backend/model/Posts.java
@@ -0,0 +1,30 @@
+package com.urban.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Lob
+    @Column(columnDefinition = "bytea")
+    private byte[] photo;
+
+}
---
+++ backend/src/main/java/com/urban/backend/model/UserInfo.java
@@ -0,0 +1,37 @@
+package com.urban.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_info")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(name = "birth_date")
+    private Instant birthDate;
+
+    @Lob
+    @Column(columnDefinition = "bytea")
+    private byte[] photo;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
